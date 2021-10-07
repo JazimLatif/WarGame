aa94578fbcf937acc818da237d43601be19c1f7c
JAZIM LATIF <jclatif99@gmail.com>
2021-10-07 14:34:56 +0100

    war

--- src/execute.js
+++ src/execute.js
@@ -1,4 +1,5 @@
-const Game = require("../src/Game.js"); const Player = require("../src/Player.js");
+const Game = require("../src/Game.js");
+const Player = require("../src/Player.js");
 let game = new Game;
 Game.deal();
 while((player1.hand.length() || player2.hand.length() ) != 0){Game.playRound(); }
