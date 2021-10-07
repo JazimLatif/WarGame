84a9ce657eaa1e65d653ffcc6b02be6b8b0a8a69
imcalled <queenieyear@gmail.com>
2021-10-07 10:43:41 +0100

    Added win prototype method in Player object class

--- src/Player.js
+++ src/Player.js
@@ -4,5 +4,15 @@ const Player = function(playerName){
       isWinner = null;
 }
 
-module.exports = Player
+Player.prototype.win = function(numCardsWon, winningPlayerHand, losingPlayerHand) {
+      for(i = 0; i <= numCardsWon; i++) {
+            winningPlayerHand.push(winningPlayerHand.shift());
+            winningPlayerHand.push(losingPlayerHand[i]);
+      }
+}
 
+Player.prototype.lose = function (numCardsLost) {
+
+}
+
+module.exports = Player
