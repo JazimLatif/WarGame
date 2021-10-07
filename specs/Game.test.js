93e24390a4bfebe4879459832659a1bd3540f2e8
JAZIM LATIF <jclatif99@gmail.com>
2021-10-07 10:37:24 +0100

    War Game

--- specs/Game.test.js
+++ specs/Game.test.js
@@ -1,8 +1,26 @@
+const Game = require("../src/Game")
+const Player = require("../src/Player")
 
 require("../src/Game")
 
-test("See if deck deals 26 cards to each player", function(){
+test("See if deal function deals out all 52 cards (leaving deck length at 0)", function(){
     //Assure
-    const testDeck = 
-
+    const testGame = new Game("John","Sally")
+    testGame.deal()
+    expect(deck.length).toBe(0)
 })
+
+
+test("See if player 1 receives half the deck (26 cards)", function(){
+    //Assure
+    const testGame = new Game("John","Sally")
+    testGame.deal()
+    expect(testGame.player1.hand.length).toBe(26)
+ })
+
+ test("See if player 1 receives half the deck (26 cards)", function(){
+    //Assure
+    const testGame = new Game("John","Sally")
+    testGame.deal()
+    expect(testGame.player2.hand.length).toBe(26)
+ })
--- src/Game.js
+++ src/Game.js
@@ -1,44 +1,44 @@
 const Player = require("./Player.js")
 
-const Game = function(player1,player2){
-    this.player1= Player.player1
-    this.player2= Player.player2
+const Game = function(){
+    this.player1= new Player("Jazim")
+    this.player2= new Player("Josh")
 }
 
 deck = [2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,11,11,11,11,12,12,12,12,13,13,13,13,14,14,14,14]
 
-player1Hand.Player = []
-player2Hand.Player = []
 
 Game.prototype.deal = function(){
     for (i=1; i<= 26; i++){
         const random = Math.floor(Math.random() * deck.length);
         const val = deck.splice(random,1)[0];
-        player1Hand.push(val)
+        this.player1.hand.push(val)
         const random2 = Math.floor(Math.random() * deck.length);
         const val2 = deck.splice(random2,1)[0];
-        player2Hand.push(val2)
+        this.player2.hand.push(val2)
         }}
 
-
-
+// testG = new Game("James","Jazim")
+// testG.deal()
+// console.log(testG.player1.hand)
+// console.log(testG.player2.hand)
 //if cards have same value go to war mode
-function war() {
-    //if cards still have the same value, continue round and draw 4 extra cards
-    let i = 1;
-    while(player.player2Hand[i] == player.player1Hand[i]) {
-        console.log("It's a draw.. The war continues!");
-        i += 4;
-        //increase index of array and shift/push at the end
-    }
-    else if(player.player1Hand[i]>player.player2Hand[i]){
-        console.log("Player 1 wins the war!");
-        player2Hand.shift()
+// Game.prototype.war= function(){
+//     //if cards still have the same value, continue round and draw 4 extra cards
+//     let i = 1;
+//     while(player.player2Hand[i] === player.player1Hand[i]) {
+//         console.log("It's a draw.. The war continues!");
+//         i += 4;
+//         //increase index of array and shift/push at the end
+//     }
+//     else if(player.player1Hand[i]>player.player2Hand[i]){
+//         console.log("Player 1 wins the war!");
+//         player2Hand.shift()
 
-    } else if (player.player2Hand[i]>player.player1Hand[i]) {
-        console.log("Player 2 wins the war!");
-    }
-}
+//     } else if (player.player2Hand[i]>player.player1Hand[i]) {
+//         console.log("Player 2 wins the war!");
+//     }
+// }
 
 module.exports = Game
 
